08e9489a481a0825c3a96cbcf5f2c670f03688f5
sergio060422 <mr.sergiolooolll@gmail.com>
2024-10-31 13:43:38 -0600

    Add files via upload

--- p.js
+++ p.js
@@ -5,7 +5,7 @@ function create_grid() {
   let wd = document.getElementsByTagName("hr")[0].offsetWidth;
 
   if(wd <= 800){
-      n = 3;
+      n = 10;
   }
     
   for(let i = 1; i <= n * n; i++){
